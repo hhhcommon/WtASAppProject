14276b27f8d89d8392e53f7c6f424fe37799e05d
wangzhitj <wangzhexilove@sina.com>
2016-11-10 17:10:02 +0800

    Merge branch 'master' of https://github.com/woting2015/WtASAppProject
    Conflicts:
    	woting/src/main/java/com/woting/common/constant/StringConstant.java

--- woting/src/main/java/com/woting/ui/mine/person/updatepersonnews/UpdatePersonActivity.java
+++ woting/src/main/java/com/woting/ui/mine/person/updatepersonnews/UpdatePersonActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,20 +18,34 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.woting.R;
 import com.woting.common.application.BSApplication;
+import com.woting.common.config.GlobalConfig;
 import com.woting.common.constant.StringConstant;
 import com.woting.common.util.TimeUtils;
 import com.woting.common.util.ToastUtils;
+import com.woting.common.volley.VolleyCallback;
+import com.woting.common.volley.VolleyRequest;
 import com.woting.common.widgetui.pickview.LoopView;
 import com.woting.common.widgetui.pickview.OnItemSelectedListener;
 import com.woting.ui.baseactivity.BaseActivity;
+import com.woting.ui.home.program.fenlei.model.Catalog;
+import com.woting.ui.home.program.fenlei.model.CatalogName;
 import com.woting.ui.mine.person.updatepersonnews.model.personModel;
 import com.woting.ui.mine.person.updatepersonnews.util.DateUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 修改个人信息(还未完成，后台接口暂时没有)
@@ -81,6 +96,13 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
     private TextView tv_region;
     private EditText tv_mail;
     private EditText tv_signature;
+    private String tag = "UPDATE_PERSON_VOLLEY_REQUEST_CANCEL_TAG";
+    private boolean isCancelRequest;
+    private Map<String,List<CatalogName>> TempMap;
+    protected Map<String,List<String>> positionMap=new HashMap<>(); //主数据Map
+    protected ArrayList<String> provinceList; //一级菜单list
+    protected LinearLayout lin_Area;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,8 +110,15 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         setContentView(R.layout.activity_updateperson);
         initView();
         setValueByPrefer();
+        if (GlobalConfig.CURRENT_NETWORK_STATE_TYPE != -1) {
+            send();
+        } else {
+            ToastUtils.show_allways(context, "网络失败，请检查网络");
+        }
 
     }
+
+
 
     private void setValueByPrefer() {
         // 账号
@@ -112,7 +141,6 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         }else{
             gender="xb002";
         }
-
         changViewGender();
 
         // 生日
@@ -142,7 +170,7 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
     private void initView() {
         findViewById(R.id.head_left_btn).setOnClickListener(this);
         findViewById(R.id.lin_age).setOnClickListener(this);
-        findViewById(R.id.lin_area).setOnClickListener(this);
+        lin_Area=(LinearLayout) findViewById(R.id.lin_area);
 
         lin_gender_man=(LinearLayout) findViewById(R.id.lin_gender_man);
         lin_gender_man.setOnClickListener(this);
@@ -158,10 +186,100 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         tv_signature=(EditText)findViewById(R.id.tv_signature);
 
         datePickerDialog();
-        cityPickerDialog();
-
-
     }
+
+    //获取地理位置
+    private void send() {
+        JSONObject jsonObject = VolleyRequest.getJsonObject(context);
+        try {
+            jsonObject.put("CatalogType", "2");
+            jsonObject.put("ResultType", "1");
+            jsonObject.put("RelLevel", "0");
+
+            jsonObject.put("Page", "1");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        VolleyRequest.RequestPost(GlobalConfig.getCatalogUrl, tag, jsonObject, new VolleyCallback() {
+
+
+            @Override
+            protected void requestSuccess(JSONObject result) {
+                Log.e("获取城市列表", "" + result.toString());
+                if (isCancelRequest) {
+                    return;
+                }
+                try {
+                    String ReturnType = result.getString("ReturnType");
+                    // 根据返回值来对程序进行解析
+                    if (ReturnType != null) {
+                        if (ReturnType.equals("1001")) {
+                            try {
+                                String ResultList = result.getString("CatalogData");
+                                Catalog SubList_all = new Gson().fromJson(ResultList, new TypeToken<Catalog>() {
+                                }.getType());
+                                List<CatalogName> s = SubList_all.getSubCata();
+                                if (s != null && s.size() > 0) {
+                                    cityPickerDialog();
+                                    lin_Area.setOnClickListener(UpdatePersonActivity.this);
+                                    TempMap=new HashMap<>();
+                                    provinceList=new ArrayList<>();
+                                    for(int i = 0; i < s.size(); i++){
+                                        if(!TextUtils.isEmpty(s.get(i).getCatalogId())&&!TextUtils.isEmpty(s.get(i).getCatalogName())
+                                                &&s.get(i).getSubCata()!=null&&s.get(i).getSubCata().size()>0){
+                                            /*
+                                            CatalogName  mFenLeiName=new CatalogName();
+                                            mFenLeiName.setCatalogId(s.get(i).getCatalogId());
+                                            mFenLeiName.setCatalogName(s.get(i).getCatalogName());
+                                            provinceCatalogNameList.add(mFenLeiName);*/
+                                            provinceList.add(s.get(i).getCatalogName());
+                                            List<CatalogName> myList=s.get(i).getSubCata();
+                                            TempMap.put(s.get(i).getCatalogName(),myList);
+                                        }
+                                    }
+                                    if(TempMap.size()>0){
+                                            for(int i=0;i<provinceList.size();i++){
+                                                List<CatalogName> mList=new ArrayList<>();
+                                                mList=TempMap.get(provinceList.get(i));
+                                                ArrayList<String> cityList=new ArrayList<>();
+                                                for(int j=0;j<mList.size();j++){
+                                                    if(mList.get(j).getCatalogName()!=null){
+                                                        cityList.add(mList.get(j).getCatalogName());
+                                                    }
+                                                }
+                                                positionMap.put(provinceList.get(i),cityList);
+                                            }
+                                    }
+                                } else {
+                                    Log.e("", "获取城市列表为空");
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } else if (ReturnType.equals("1002")) {
+                            ToastUtils.show_short(context, "无此分类信息");
+                        } else if (ReturnType.equals("1003")) {
+                            ToastUtils.show_short(context, "分类不存在");
+                        } else if (ReturnType.equals("1011")) {
+                            ToastUtils.show_short(context, "当前暂无分类");
+                        } else if (ReturnType.equals("T")) {
+                            ToastUtils.show_short(context, "获取列表异常");
+                        }
+                    } else {
+                        ToastUtils.show_short(context, "数据获取异常，请稍候重试");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected void requestError(VolleyError error) {
+
+            }
+        });
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -212,51 +330,6 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         }
     }
 
-    private void send() {
-       /* dialog = DialogUtils.Dialogph(context, "正在提交请求");
-        JSONObject jsonObject = VolleyRequest.getJsonObject(context);
-        try {
-            jsonObject.put("OldPassword", oldPassword);// 待改
-            jsonObject.put("newPassword", newPassword);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        VolleyRequest.RequestPost(GlobalConfig.modifyPasswordUrl, tag, jsonObject, new VolleyCallback() {
-            private String ReturnType;
-            private String Message;
-
-            @Override
-            protected void requestSuccess(JSONObject result) {
-                if (dialog != null) dialog.dismiss();
-                if (isCancelRequest) return;
-                try {
-                    ReturnType = result.getString("ReturnType");
-                    Message = result.getString("Message");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                if (ReturnType != null && ReturnType.equals("1001")) {
-                    ToastUtils.show_allways(context, "密码修改成功");
-                    finish();
-                }
-                if (ReturnType != null && ReturnType.equals("1002")) {
-                    ToastUtils.show_allways(context, "" + Message);
-                } else {
-                    if (Message != null && !Message.trim().equals("")) {
-                        ToastUtils.show_allways(context, Message + "");
-                    }
-                }
-            }
-
-            @Override
-            protected void requestError(VolleyError error) {
-                if (dialog != null) dialog.dismiss();
-                ToastUtils.showVolleyError(context);
-            }
-        });*/
-
-    }
 
     /**
      * 日期选择框
@@ -423,6 +496,10 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         pick_Province=(LoopView)dialog.findViewById(R.id.pick_province);
         pick_City=(LoopView)dialog.findViewById(R.id.pick_city);
 
+        pick_Province.setItems(provinceList);
+        pick_City.setItems(provinceList);
+
+
         TextView tv_confirm = (TextView) dialog.findViewById(R.id.tv_confirm);
         TextView tv_cancel = (TextView) dialog.findViewById(R.id.tv_cancel);
         cityDialog = new Dialog(context, R.style.MyDialog);
@@ -439,11 +516,6 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
         window.setWindowAnimations(R.style.sharestyle);
         cityDialog.setCanceledOnTouchOutside(true);
         cityDialog.getWindow().setBackgroundDrawableResource(R.color.dialog);
-       /* dialog1 = DialogUtils.Dialogphnoshow(context, "通讯中", dialog1);
-        Config.dialog = dialog1;
-        final List<ShareModel> mList = ShareUtils.getShareModelList();
-        ImageAdapter shareAdapter = new ImageAdapter(context, mList);
-        mGallery.setAdapter(shareAdapter);*/
         tv_confirm.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -476,7 +548,7 @@ public class UpdatePersonActivity extends BaseActivity implements OnClickListene
 
     @Override
     protected void onDestroy(){
-
+        isCancelRequest = VolleyRequest.cancelRequest(tag);
         super.onDestroy();
     }
 
