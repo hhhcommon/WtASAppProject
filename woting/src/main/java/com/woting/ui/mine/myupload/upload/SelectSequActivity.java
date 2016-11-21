4c122a941cbae7a516b6917a19f3c59a7db66e09
54PirateGit <18179739967@163.com>
2016-11-21 19:36:30 +0800

    commit  11/20    _1

---
+++ woting/src/main/java/com/woting/ui/mine/myupload/upload/SelectSequActivity.java
@@ -0,0 +1,19 @@
+package com.woting.ui.mine.myupload.upload;
+
+import android.os.Bundle;
+
+import com.woting.R;
+import com.woting.ui.baseactivity.AppBaseActivity;
+
+/**
+ * 选择专辑
+ * Created by Administrator on 2016/11/21.
+ */
+public class SelectSequActivity extends AppBaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_sequ);
+    }
+}
