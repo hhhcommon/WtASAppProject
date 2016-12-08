cc07bd202a47ff670c8dc98658f74a651233aa97
54PirateGit <18179739967@163.com>
2016-12-08 19:02:19 +0800

    commit  12/08    _3

--- woting/src/main/java/com/woting/common/widgetui/pulltorefresh/MyListener.java
+++ woting/src/main/java/com/woting/common/widgetui/pulltorefresh/MyListener.java
@@ -6,39 +6,29 @@ import android.os.Message;
 
 import com.woting.common.widgetui.pulltorefresh.PullToRefreshLayout.OnRefreshListener;
 
+public class MyListener implements OnRefreshListener {
 
+    @Override
+    public void onRefresh(final PullToRefreshLayout pullToRefreshLayout) {
+        // 下拉刷新操作
+        new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                // 千万别忘了告诉控件刷新完毕了哦！
+                pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
+            }
+        }.sendEmptyMessageDelayed(0, 5000);
+    }
 
-public class MyListener implements OnRefreshListener
-{
-
-	@Override
-	public void onRefresh(final PullToRefreshLayout pullToRefreshLayout)
-	{
-		// 下拉刷新操作
-		new Handler()
-		{
-			@Override
-			public void handleMessage(Message msg)
-			{
-				// 千万别忘了告诉控件刷新完毕了哦！
-				pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
-			}
-		}.sendEmptyMessageDelayed(0, 5000);
-	}
-
-	@Override
-	public void onLoadMore(final PullToRefreshLayout pullToRefreshLayout)
-	{
-		// 加载操作
-		new Handler()
-		{
-			@Override
-			public void handleMessage(Message msg)
-			{
-				// 千万别忘了告诉控件加载完毕了哦！
-				pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
-			}
-		}.sendEmptyMessageDelayed(0, 5000);
-	}
-
+    @Override
+    public void onLoadMore(final PullToRefreshLayout pullToRefreshLayout) {
+        // 加载操作
+        new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                // 千万别忘了告诉控件加载完毕了哦！
+                pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
+            }
+        }.sendEmptyMessageDelayed(0, 5000);
+    }
 }
