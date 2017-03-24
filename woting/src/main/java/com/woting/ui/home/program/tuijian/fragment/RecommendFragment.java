61dc0899d4d67a2c4c854b12aaefe65bafb3b38b
54PirateGit <18179739967@163.com>
2017-03-24 17:25:30 +0800

    commit  03/24    _6

--- woting/src/main/java/com/woting/ui/home/program/tuijian/fragment/RecommendFragment.java
+++ woting/src/main/java/com/woting/ui/home/program/tuijian/fragment/RecommendFragment.java
@@ -20,6 +20,7 @@ import com.google.gson.reflect.TypeToken;
 import com.woting.R;
 import com.woting.common.config.GlobalConfig;
 import com.woting.common.constant.BroadcastConstants;
+import com.woting.common.constant.IntegerConstant;
 import com.woting.common.constant.StringConstant;
 import com.woting.common.util.CommonUtils;
 import com.woting.common.util.DialogUtils;
@@ -293,7 +294,7 @@ public class RecommendFragment extends Fragment implements TipView.WhiteViewClic
                     } else if (MediaType.equals("SEQU")) {
                         AlbumFragment fragment = new AlbumFragment();
                         Bundle bundle = new Bundle();
-                        bundle.putInt("fromType", 2);
+                        bundle.putInt(StringConstant.FROM_TYPE, IntegerConstant.TAG_HOME);
                         bundle.putString("type", "recommend");
                         bundle.putSerializable("list", newList.get(position - 2));
                         fragment.setArguments(bundle);
