bd6a6f8b0a4cf8969fd6e1842497d24743ca1f38
54PirateGit <18179739967@163.com>
2017-03-27 13:33:04 +0800

    commit  03/27    _2

--- woting/src/main/java/Test/ImageViewShowActivity.java
+++ woting/src/main/java/Test/ImageViewShowActivity.java
@@ -1,164 +1,166 @@
-//package Test;
-//
-//import java.util.ArrayList;
-//
-//import android.os.Bundle;
-//import android.os.Parcelable;
-//import android.support.v4.view.PagerAdapter;
-//import android.support.v4.view.ViewPager;
-//import android.support.v4.view.ViewPager.OnPageChangeListener;
-//import android.view.KeyEvent;
-//import android.view.LayoutInflater;
-//import android.view.MotionEvent;
-//import android.view.View;
-//import android.view.ViewGroup.LayoutParams;
-//import android.widget.ImageView;
-//import android.widget.LinearLayout;
-//import android.widget.TextView;
-//
-//import com.woting.R;
-//import com.woting.common.util.PhoneMessage;
-//import com.woting.ui.baseactivity.BaseActivity;
-//
-//public class ImageViewShowActivity extends BaseActivity implements View.OnClickListener {
-//	private TextView tv_num;
-//	private ViewPager mViewPager;
-//	private String list;
-//	private int num;
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_image_show);
-//
-//		num=this.getIntent().getIntExtra("num", 0);
-//		list = this.getIntent().getStringExtra("list");
-//
-//		tv_num=(TextView)findViewById(R.id.tv_num);
-//		findViewById(R.id.tv_num).setOnClickListener(this);
-//		mViewPager = (ViewPager)findViewById(R.id.viewpager);
-//		mViewPager.setOffscreenPageLimit(1);
-//		setData();
-//	}
-//
-//	@Override
-//	public void onClick(View v) {
-//		switch (v.getId()) {
-//		case R.id.head_left_btn:
-//			finish();
-//		break;
-//		}
-//	}
-//
-//	private void setData() {
-//		final ArrayList<View> mViews;
-//		if(list!=null&&!list.trim().equals("")){
-//			if(list.contains("YMKJ")){
-//				String url[] = list.split("YMKJ");
-//				  mViews = new ArrayList<View>();
-//				for(int i=0;i<url.length;i++){
-//					View layout = LayoutInflater.from(this).inflate(R.layout.layout_image, null);
-//					ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
-//					LayoutParams pra=imageView.getLayoutParams();
-//					pra.width= PhoneMessage.ScreenWidth;
-//					imageView.setLayoutParams(pra);
+package Test;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.woting.R;
+import com.woting.common.util.PhoneMessage;
+import com.woting.ui.baseactivity.BaseActivity;
+
+import java.util.ArrayList;
+
+public class ImageViewShowActivity extends BaseActivity implements View.OnClickListener {
+    private TextView tv_num;
+    private ViewPager mViewPager;
+    private String list;
+    private int num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_show);
+
+        num = this.getIntent().getIntExtra("num", 0);
+        list = this.getIntent().getStringExtra("list");
+
+        tv_num = (TextView) findViewById(R.id.tv_num);
+        findViewById(R.id.tv_num).setOnClickListener(this);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setOffscreenPageLimit(1);
+        setData();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.head_left_btn:
+                finish();
+                break;
+        }
+    }
+
+    private void setData() {
+        final ArrayList<View> mViews;
+        if (list != null && !list.trim().equals("")) {
+            if (list.contains("YMKJ")) {
+                String url[] = list.split("YMKJ");
+                mViews = new ArrayList<>();
+                for (int i = 0; i < url.length; i++) {
+                    View layout = LayoutInflater.from(this).inflate(R.layout.layout_image, null);
+                    ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
+                    LayoutParams pra = imageView.getLayoutParams();
+                    pra.width = PhoneMessage.ScreenWidth;
+                    imageView.setLayoutParams(pra);
 //					imageLoader.DisplayImage(url[i].replace( "\\/", "/"), imageView, false, false, null,null);
-//					mViews.add(layout);
-//				}
-//				tv_num.setText("1/"+mViews.size());
-//
-//			}else{
-//				mViews = new ArrayList<View>();
-//				View layout = LayoutInflater.from(this).inflate(R.layout.layout_image, null);
-//				ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
+                    mViews.add(layout);
+                }
+                tv_num.setText("1/" + mViews.size());
+
+            } else {
+                mViews = new ArrayList<>();
+                View layout = LayoutInflater.from(this).inflate(R.layout.layout_image, null);
+                ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
 //				imageLoader.DisplayImage(list.replace( "\\/", "/"), imageView, false, false, null,null);
-//				LayoutParams pra=imageView.getLayoutParams();
-//				pra.width=PhoneMessage.ScreenWidth;
-//				imageView.setLayoutParams(pra);
-//				mViews.add(layout);
-//				tv_num.setText("1/"+mViews.size());
-//			}
-//			tv_num.setText(String.valueOf(num+1)+"/"+mViews.size());
-//			mViewPager.setAdapter(new MyPagerAdapter());
-//			mViewPager.setCurrentItem(num);
-//			mViewPager.setOnTouchListener(new View.OnTouchListener() {
-//
-//				@Override
-//				public boolean onTouch(View v, MotionEvent event) {
-//					v.getParent().requestDisallowInterceptTouchEvent(true);
-//					return false;
-//				}
-//
-//			});
-//
-//			mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
-//				@Override
-//				public void onPageSelected(int arg0) {
-//				}
-//
-//				@Override
-//				public void onPageScrolled(int arg0, float arg1, int arg2) {
-//					tv_num.setText((arg0+1)+"/"+mViews.size());
-//					mViewPager.getParent().requestDisallowInterceptTouchEvent(true);
-//				}
-//
-//				@Override
-//				public void onPageScrollStateChanged(int arg0) {
-//				}
-//			});
-//		}else{
-//			mViewPager.setVisibility(View.GONE);
-//		}
-//
-//
-//	}
-//
-//	private class MyPagerAdapter extends PagerAdapter{
-//		@Override
-//		public void destroyItem(View v, int position, Object obj) {
+                LayoutParams pra = imageView.getLayoutParams();
+                pra.width = PhoneMessage.ScreenWidth;
+                imageView.setLayoutParams(pra);
+                mViews.add(layout);
+                tv_num.setText("1/" + mViews.size());
+            }
+            tv_num.setText(String.valueOf(num + 1) + "/" + mViews.size());
+            mViewPager.setAdapter(new MyPagerAdapter());
+            mViewPager.setCurrentItem(num);
+            mViewPager.setOnTouchListener(new View.OnTouchListener() {
+
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    return false;
+                }
+
+            });
+
+            mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+                @Override
+                public void onPageSelected(int arg0) {
+                }
+
+                @Override
+                public void onPageScrolled(int arg0, float arg1, int arg2) {
+                    tv_num.setText((arg0 + 1) + "/" + mViews.size());
+                    mViewPager.getParent().requestDisallowInterceptTouchEvent(true);
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int arg0) {
+                }
+            });
+        } else {
+            mViewPager.setVisibility(View.GONE);
+        }
+
+
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+        @Override
+        public void destroyItem(View v, int position, Object obj) {
 //			((ViewPager)v).removeView(mViews.get(position));
-//		}
-//
-//		@Override
-//		public void finishUpdate(View arg0) {
-//		}
-//
-//		@Override
-//		public int getCount() {
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+        }
+
+        @Override
+        public int getCount() {
 //			return mViews.size();
-//		}
-//
-//		@Override
-//		public Object instantiateItem(View v, int position) {
+            return 0;
+        }
+
+        @Override
+        public Object instantiateItem(View v, int position) {
 //			((ViewPager)v).addView(mViews.get(position));
 //			return mViews.get(position);
-//		}
-//
-//		@Override
-//		public boolean isViewFromObject(View arg0, Object arg1) {
-//			return arg0 == arg1;
-//		}
-//
-//		@Override
-//		public void restoreState(Parcelable arg0, ClassLoader arg1) {
-//		}
-//
-//		@Override
-//		public Parcelable saveState() {
-//			return null;
-//		}
-//
-//		@Override
-//		public void startUpdate(View arg0) {
-//		}
-//	}
-//
-//	@Override
-//	public boolean onKeyDown(int keyCode,KeyEvent event) {
-//		// 是否触发按键为back键
-//		if (keyCode == KeyEvent.KEYCODE_BACK) {
-//			finish();
-//		}
-//		return super.onKeyDown(keyCode,event);
-//	}
-//
-//}
+            return null;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // 是否触发按键为back键
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
--- woting/src/main/java/com/woting/ui/home/program/tuijian/adapter/RecommendListAdapter.java
+++ woting/src/main/java/com/woting/ui/home/program/tuijian/adapter/RecommendListAdapter.java
@@ -11,140 +11,143 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.woting.R;
-import com.woting.common.util.AssembleImageUrlUtils;
-import com.woting.ui.home.program.fmlist.model.RankInfo;
 import com.woting.common.config.GlobalConfig;
+import com.woting.common.util.AssembleImageUrlUtils;
 import com.woting.common.util.BitmapUtils;
+import com.woting.ui.home.program.fmlist.model.RankInfo;
 
 import java.util.List;
 
+/**
+ * 推荐数据展示
+ */
 public class RecommendListAdapter extends BaseAdapter {
-	private List<RankInfo> list;
-	private Context context;
-	private Bitmap bmp;
-	private boolean isHintVisibility;
+    private List<RankInfo> list;
+    private Context context;
+    private Bitmap bmp;
+    private boolean isHintVisibility;
 
-	public RecommendListAdapter(Context context, List<RankInfo> list, boolean isHintVisibility) {
-		this.context = context;
-		this.list = list;
-		this.isHintVisibility = isHintVisibility;
-	}
+    public RecommendListAdapter(Context context, List<RankInfo> list, boolean isHintVisibility) {
+        this.context = context;
+        this.list = list;
+        this.isHintVisibility = isHintVisibility;
+    }
 
-	@Override
-	public int getCount() {
-		return list.size();
-	}
+    @Override
+    public int getCount() {
+        return list.size();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return list.get(position);
-	}
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	@Override
-	public View getView(final int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		if (convertView == null) {
-			convertView = LayoutInflater.from(context).inflate(R.layout.adapter_fragment_recommend, null);
-			holder = new ViewHolder();
-			holder.textview_ranktitle = (TextView) convertView.findViewById(R.id.RankTitle);// 台名
-			holder.imageview_rankimage = (ImageView) convertView.findViewById(R.id.RankImageUrl);// 电台图标
-			holder.mTv_number = (TextView) convertView.findViewById(R.id.tv_num);
-			holder.textRankContent = (TextView) convertView.findViewById(R.id.RankContent);
-			holder.textTotal = (TextView) convertView.findViewById(R.id.tv_total);
-			holder.imageNumberTime = (ImageView) convertView.findViewById(R.id.image_number_time);
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.adapter_fragment_recommend, null);
 
-			holder.img_zhezhao = (ImageView) convertView.findViewById(R.id.img_zhezhao);
-			Bitmap bmp_zhezhao = BitmapUtils.readBitMap(context, R.mipmap.wt_6_b_y_b);
-			holder.img_zhezhao.setImageBitmap(bmp_zhezhao);
-			
-			holder.imageHintVisibility = (ImageView) convertView.findViewById(R.id.image_hint_visibility);
-			if(isHintVisibility){
-				holder.imageHintVisibility.setVisibility(View.GONE);
-			}else{
-				holder.imageHintVisibility.setVisibility(View.VISIBLE);
-			}
-			 bmp = BitmapUtils.readBitMap(context, R.mipmap.wt_image_playertx);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
+            // 六边形封面图片遮罩
+            Bitmap bitmapMask = BitmapUtils.readBitMap(context, R.mipmap.wt_6_b_y_b);
+            holder.imageMask = (ImageView) convertView.findViewById(R.id.img_zhezhao);
+            holder.imageMask.setImageBitmap(bitmapMask);
 
-	
+            holder.imageCover = (ImageView) convertView.findViewById(R.id.RankImageUrl);// 封面图片
+            holder.textTitle = (TextView) convertView.findViewById(R.id.RankTitle);// 标题
+            holder.mTv_number = (TextView) convertView.findViewById(R.id.tv_num);
+            holder.textRankContent = (TextView) convertView.findViewById(R.id.RankContent);
+            holder.textTotal = (TextView) convertView.findViewById(R.id.tv_total);
+            holder.imageNumberTime = (ImageView) convertView.findViewById(R.id.image_number_time);
 
-		RankInfo lists = list.get(position);
+            holder.imageHintVisibility = (ImageView) convertView.findViewById(R.id.image_hint_visibility);
+            if (isHintVisibility) {
+                holder.imageHintVisibility.setVisibility(View.GONE);
+            } else {
+                holder.imageHintVisibility.setVisibility(View.VISIBLE);
+            }
+            bmp = BitmapUtils.readBitMap(context, R.mipmap.wt_image_playertx);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-		if (lists.getContentName() == null || lists.getContentName().equals("")) {
-			holder.textview_ranktitle.setText("未知");
-		} else {
-			holder.textview_ranktitle.setText(lists.getContentName());
-		}
+        RankInfo lists = list.get(position);
 
-		if (lists.getContentImg() == null || lists.getContentImg().equals("null")|| lists.getContentImg().trim().equals("")) {
-			holder.imageview_rankimage.setImageBitmap(bmp);
-		} else {
-			String url;
-			if(lists.getContentImg().startsWith("http")){
-				url =  lists.getContentImg();
-			}else{
-				url = GlobalConfig.imageurl + lists.getContentImg();
-			}
-			url=AssembleImageUrlUtils.assembleImageUrl180(url);
-			Picasso.with(context).load(url.replace("\\/", "/")).into(holder.imageview_rankimage);
-		}
+        // 封面图片
+        String contentImg = lists.getContentImg();
+        if (contentImg == null || contentImg.equals("null") || contentImg.trim().equals("")) {
+            holder.imageCover.setImageBitmap(bmp);
+        } else {
+            if (!contentImg.startsWith("http")) {
+                contentImg = GlobalConfig.imageurl + contentImg;
+            }
+            contentImg = AssembleImageUrlUtils.assembleImageUrl180(contentImg);
+            Picasso.with(context).load(contentImg.replace("\\/", "/")).into(holder.imageCover);
+        }
 
-		if(lists != null || lists.getMediaType() != null){
-			if (lists.getMediaType().equals("SEQU")) {
-				holder.imageNumberTime.setImageResource(R.mipmap.image_program_number);
-				if (lists.getContentSubCount() == null || lists.getContentSubCount().equals("")
-						|| lists.getContentSubCount().equals("null")) {
-					holder.textTotal.setText("0" + "集");
-				} else {
-					holder.textTotal.setText(lists.getContentSubCount() + "集");
-				}
-			} else if(lists.getMediaType().equals("RADIO") || lists.getMediaType().equals("AUDIO")) {
-				holder.imageNumberTime.setImageResource(R.mipmap.image_program_time);
-				//节目时长
-				if (lists.getContentTimes() == null|| lists.getContentTimes().equals("") || lists.getContentTimes().equals("null")) {
-					holder.textTotal.setText(context.getString(R.string.play_time));
-				} else {
-					long minute = Long.valueOf(lists.getContentTimes()) / (1000 * 60);
+        // 标题
+        String contentName = lists.getContentName();
+        if (contentName == null || contentName.equals("")) {
+            contentName = "未知";
+        }
+        holder.textTitle.setText(contentName);
+
+        if (lists != null || lists.getMediaType() != null) {
+            if (lists.getMediaType().equals("SEQU")) {
+                holder.imageNumberTime.setImageResource(R.mipmap.image_program_number);
+                if (lists.getContentSubCount() == null || lists.getContentSubCount().equals("")
+                        || lists.getContentSubCount().equals("null")) {
+                    holder.textTotal.setText("0" + "集");
+                } else {
+                    holder.textTotal.setText(lists.getContentSubCount() + "集");
+                }
+            } else if (lists.getMediaType().equals("RADIO") || lists.getMediaType().equals("AUDIO")) {
+                holder.imageNumberTime.setImageResource(R.mipmap.image_program_time);
+                // 节目时长
+                if (lists.getContentTimes() == null || lists.getContentTimes().equals("") || lists.getContentTimes().equals("null")) {
+                    holder.textTotal.setText(context.getString(R.string.play_time));
+                } else {
+                    long minute = Long.valueOf(lists.getContentTimes()) / (1000 * 60);
                     long second = (Long.valueOf(lists.getContentTimes()) / 1000) % 60;
-					if(second < 10){
-						holder.textTotal.setText(minute + "\'" + " " + "0" + second + "\"");
-					}else{
-						holder.textTotal.setText(minute + "\'" + " " + second + "\"");
-					}
-				}
-			}
-		}
-		if (lists.getPlayCount() == null || lists.getPlayCount().equals("") || lists.getPlayCount().equals("null")) {
-			holder.mTv_number.setText("0");
-		} else {
-			holder.mTv_number.setText(lists.getPlayCount());
-		}
-		if (lists.getContentPub() == null || lists.getContentPub().equals("") || lists.getContentPub().equals("null")) {
-			holder.textRankContent.setText("未知");
-		} else {
-			holder.textRankContent.setText(lists.getContentPub());
-		}
+                    if (second < 10) {
+                        holder.textTotal.setText(minute + "\'" + " " + "0" + second + "\"");
+                    } else {
+                        holder.textTotal.setText(minute + "\'" + " " + second + "\"");
+                    }
+                }
+            }
+        }
+        if (lists.getPlayCount() == null || lists.getPlayCount().equals("") || lists.getPlayCount().equals("null")) {
+            holder.mTv_number.setText("0");
+        } else {
+            holder.mTv_number.setText(lists.getPlayCount());
+        }
+        if (lists.getContentPub() == null || lists.getContentPub().equals("") || lists.getContentPub().equals("null")) {
+            holder.textRankContent.setText("未知");
+        } else {
+            holder.textRankContent.setText(lists.getContentPub());
+        }
 
-		return convertView;
-	}
-	
-	static class  ViewHolder {
-		public ImageView imageview_rankimage;
-		public TextView textview_ranktitle;
-		public TextView mTv_number;
-		public TextView textRankContent;
-		public TextView textTotal;
-		public ImageView imageHintVisibility;
-		public ImageView imageNumberTime;
-		public ImageView img_zhezhao;
-	}
-	
+        return convertView;
+    }
+
+    static class ViewHolder {
+        public ImageView imageMask;// 六边形封面图片遮罩
+        public ImageView imageCover;// 封面图片
+        public TextView textTitle;// 标题
+        public TextView mTv_number;
+        public TextView textRankContent;
+        public TextView textTotal;
+        public ImageView imageHintVisibility;
+        public ImageView imageNumberTime;
+    }
+
 }
